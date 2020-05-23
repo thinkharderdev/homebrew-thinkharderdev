7c9f7e8ffda61ecac0fedf0c79319152d82dd26f
thinkharderdev <1327726+thinkharderdev@users.noreply.github.com>
2020-05-23 06:08:05 -0400

    quotes

--- Formula/shield.rb
+++ Formula/shield.rb
@@ -5,7 +5,7 @@ class Shield < Formula
   sha256 "b90ee3a41b1bc5eae65d64216dfdfd8f4098cd0ccae531e98b97714a061894a9"
 
   def install
-    prefix.install hosts.orig
-    bin.install shield 
+    prefix.install "hosts.orig"
+    bin.install "shield"
   end
 end
