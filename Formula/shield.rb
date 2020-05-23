c4f332ecc5a808ac284ee0b47eb06e6dfe611d3f
thinkharderdev <1327726+thinkharderdev@users.noreply.github.com>
2020-05-23 06:03:08 -0400

    Add url and hash

--- Formula/shield.rb
+++ Formula/shield.rb
@@ -1,7 +1,7 @@
 class Shield < Formula
   desc "DNS Shield"
-  homepage ""
-  url ""
+  homepage "https://github.com/thinkharderdev/homebrew-thinkharderdev/blob/master/src/shield"
+  url "https://github.com/thinkharderdev/homebrew-thinkharderdev/raw/master/archive/shield-1.0.0.tar.gz"
   sha256 "b90ee3a41b1bc5eae65d64216dfdfd8f4098cd0ccae531e98b97714a061894a9"
 
   def install
